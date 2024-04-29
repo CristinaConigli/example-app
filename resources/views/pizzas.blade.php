00c0df77e174be3dbf94e06b714b2ce7b27bd996
CristinaConigli <coniglicristina@gmail.com>
2024-04-29 18:07:21 +0200

    v1

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gatti</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+            *{
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+            }
+        </style>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div style="background-color: black;">
+            <h1 style="color: #fff;"> Cat List</h1>
+        </div>
+        <div>
+            
+            
+
+            @for( $i=0;  $i<count($pizzas) ;  $i++)
+            <p> {{$pizzas[$i]['type']}}</p>
+            @endfor
+        </div>
+    </body>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cats', function () {
+    $pizzas=[
+        ['type'=>'hawaiian','base'=>'cheesy crust'],
+        ['type'=>'volcano','base'=>'garlic crust'],
+        ['type'=>'vegan','base'=>'thin']
+    ];
+    return view('pizzas', ['pizzas'=>$pizzas]);
+    
+});
